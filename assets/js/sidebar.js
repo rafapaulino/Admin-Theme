ab8653d7aa8ee63d79f966fb6239251c3761ce93
Rafael Paulino <rafaclasses@gmail.com>
2021-11-02 01:45:37 -0300

    error pages, blank page, html base

---
+++ assets/js/sidebar.js
@@ -0,0 +1,25 @@
+(function(window, document) {
+    'use strict';
+
+    let sidebar = (function () {
+
+        let $public = {};
+
+        $public.init = function() {
+            let button = document.getElementById("sidebarCollapse");
+            let sidebar = document.getElementsByClassName("sidebar");
+
+            button.onclick = function(e) {
+                e.preventDefault();
+                sidebar[0].classList.toggle("active");
+                button.classList.toggle("active");
+            };
+        };
+
+        return $public;
+    })();
+
+window.sidebar = sidebar;
+sidebar.init();
+
+})(window, document);
