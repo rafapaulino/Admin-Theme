f7820299c0cd6c1329fdb59cda0e3e0b65458b93
Rafael Brigagao Paulino <rafael.paulino@sesisenaisp.org.br>
2021-12-02 19:10:50 -0300

    adicionei o serialize do menu - agora os elementos são retornados em um array de objetos

--- assets/js/menu-sortable.js
+++ assets/js/menu-sortable.js
@@ -5,6 +5,7 @@
     let menuSortableApp = (function () {
 
         let $public = {};
+        let $private = {};
 
         $public.init = function () {
             $(function () {
@@ -21,7 +22,63 @@
                     isTree: true
                 });
             });
+
+            $private.serialize();
         };
+
+        $private.serialize = function () {
+            $('.sortable-serialize').on('click',function(e) {
+                e.preventDefault();  
+                
+                let results = [];
+                
+                let sorted = $('ol.sortable').sortable("serialize", {
+                  attribute: 'id'
+                });
+
+                let splits = sorted.split('&');
+
+                let index = 0;
+                
+                Array.from(splits).forEach(function(item) {
+                    index++;
+                    let parent = $private.getParent(item);
+                    let element = $private.getItem(item);
+                    
+                    let obj = new Object();
+                    obj.index = index;
+                    obj.element = element;
+                    obj.parent = parent;
+                    
+                    results.push(obj);
+                });
+
+                return results;
+            });
+        };
+
+        $private.getItem = function(item) {
+            let result = item.match(/\[(.*?)\]/g);
+            
+            if (result.length > 0) {
+                result = result[0];
+                return result.replace(/[^a-z0-9]/gi,'');
+            } else {
+                return null;
+            }
+        };
+
+        $private.getParent = function(parent) {
+            let result = parent.split('=');
+            result = result[result.length -1];
+
+            if (result !== "null")
+                return parseInt(result);
+            else 
+                return null;
+        };
+
+        
 
         return $public;
     })();
