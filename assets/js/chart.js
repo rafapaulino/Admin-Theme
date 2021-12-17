5df9c5e82142dbbc55782c937636ce2740471789
Rafael Brigagao Paulino <rafael.paulino@sesisenaisp.org.br>
2021-12-17 11:44:36 -0300

    line chart

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -8,27 +8,24 @@
         let $private = {};
 
         $public.pie = function(element, data, options) {
-            let pie = document.getElementById(element);
-            let chart = {};
-
-            if (pie !== null) {
-                chart = new Chart(pie, {
-                    type: 'pie',
-                    data: data,
-                    options: options
-                });
-            }
-
-            return chart;
+            return $private.create('pie', element, data, options);
         };
 
         $public.bar = function(element, data, options) {
-            let bar = document.getElementById(element);
+            return $private.create('bar', element, data, options);
+        };
+
+        $public.line = function(element, data, options) {
+            return $private.create('line', element, data, options);
+        };
+
+        $private.create = function(type, element, data, options) {
+            let canvas = document.getElementById(element);
             let chart = {};
 
-            if (bar !== null) {
-                chart = new Chart(bar, {
-                    type: 'bar',
+            if (canvas !== null) {
+                chart = new Chart(canvas, {
+                    type: type,
                     data: data,
                     options: options
                 });
